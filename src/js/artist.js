cb3425b4c9cb082e386c6e36e4a86157be9762a8
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-12-01 23:36:56 +0200

    add slider for artist section

---
+++ src/js/artist.js
@@ -0,0 +1,19 @@
+const artistList = document.querySelector(".js-artist__slider--list");
+const prevBtn = document.querySelector(".js-artist__button-prev");
+const nextBtn = document.querySelector(".js-artist__button-next");
+
+const slideWidth = 355;
+const slidesCount = artistList.children.length;
+let activeIndex = 0;
+
+const updateTransform = () => {
+  artistList.style.transform = `translateX(-${activeIndex * slideWidth}px)`;
+};
+
+const changeSlide = (direction) => {
+  activeIndex = (activeIndex + direction + slidesCount) % slidesCount;
+  updateTransform();
+};
+
+prevBtn.addEventListener("click", () => changeSlide(-1));
+nextBtn.addEventListener("click", () => changeSlide(1));
