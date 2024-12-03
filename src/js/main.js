95675bfb4999a7169b7875d9de7c6a2a222b3027
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-12-03 21:00:33 +0200

    add modal with styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 import "./header";
 import "./artist";
 import "./feedback";
+import "./modal";
---
+++ src/js/modal.js
@@ -0,0 +1,151 @@
+const modal = document.getElementById("modal");
+const modalContent = document.getElementById("modal-content");
+
+const toggleScroll = (shouldBlock) => {
+  document.body.style.overflow = shouldBlock ? "hidden" : "auto";
+};
+
+const updateModalContent = (contentKey, title) => {
+  const icon = title.toLowerCase();
+  const modalContentData = {
+    proceed: `<div class="modal__title-wrapper">
+      <h4 class="modal__title modal__nomargin">Your plan ${title}</h4>
+      <svg class="modal__icon" width="54" height="54">
+        <use href="./images/icons.svg#icon-${icon}"></use>
+      </svg>
+    </div>
+    <form class="modal__form" name="reviews-form">
+      <div class="modal__input--wrapper">
+        <input
+          class="modal__form__input js-input"
+          type="text"
+          name="user-name"
+          id="user-name"
+          placeholder=" "
+        />
+        <label class="modal__form__label" for="user-name">Full Name</label>
+      </div>
+      <div class="modal__input--wrapper">
+        <input
+          class="modal__form__input"
+          type="email"
+          name="user-email"
+          id="user-email"
+          placeholder=" "
+        />
+        <label class="modal__form__label" for="user-name">E-mail</label>
+      </div>
+      <div class="modal__input--wrapper">
+        <input
+          class="modal__form__input"
+          type="tel"
+          name="user-phone"
+          id="user-phone"
+          placeholder=" "
+        />
+        <label class="modal__form__label" for="user-name">Phone number*</label>
+      </div>
+
+      <button class="modal__button" type="button" data-modal-open>
+        <svg class="modal__button-icon-border">
+          <use
+            class="modal__button-icon-border-sm"
+            href="./images/icons.svg#icon-package-btn-border-sm-black"
+          ></use>
+          <use
+            class="modal__button-icon-border-lg"
+            href="./images/icons.svg#icon-package-btn-border-lg-black"
+          ></use>
+        </svg>
+        <span class="modal__button-text">Processed</span>
+        <span class="modal__button-span-icon">
+          <svg height="13" width="11">
+            <use href="./images/icons.svg#icon-arrow-sideward"></use>
+          </svg>
+        </span>
+      </button>
+    </form>
+    <p class="modal__text">
+      By sending this form I confirm that I have read and accept the
+      <span class="modal__text__span">Privacy Policy</span>
+    </p>`,
+    consultation: ` <h4 class="modal__title">Ask a question</h4>
+    <form class="modal__form" name="reviews-form">
+      <div class="modal__input--wrapper">
+        <input
+          class="modal__form__input js-input"
+          type="text"
+          name="user-name"
+          id="user-name"
+          placeholder=" "
+        />
+        <label class="modal__form__label" for="user-name">Full Name</label>
+      </div>
+      <div class="modal__input--wrapper">
+        <input
+          class="modal__form__input"
+          type="email"
+          name="user-email"
+          id="user-email"
+          placeholder=" "
+        />
+        <label class="modal__form__label" for="user-name">E-mail</label>
+      </div>
+      <div class="modal__input--wrapper">
+        <input
+          class="modal__form__input"
+          type="tel"
+          name="user-phone"
+          id="user-phone"
+          placeholder=" "
+        />
+        <label class="modal__form__label" for="user-name">Phone number*</label>
+      </div>
+
+      <button class="modal__button" type="button" data-modal-open>
+        <svg class="modal__button-icon-border">
+          <use
+            class="modal__button-icon-border-sm"
+            href="./images/icons.svg#icon-package-btn-border-sm-black"
+          ></use>
+          <use
+            class="modal__button-icon-border-lg"
+            href="./images/icons.svg#icon-package-btn-border-lg-black"
+          ></use>
+        </svg>
+        <span class="modal__button-text">Consultation</span>
+        <span class="modal__button-span-icon">
+          <svg height="13" width="11">
+            <use href="./images/icons.svg#icon-arrow-sideward"></use>
+          </svg>
+        </span>
+      </button>
+    </form>
+    <p class="modal__text">
+      By sending this form I confirm that I have read and accept the
+      <span class="modal__text__span">Privacy Policy</span>
+    </p>`,
+  };
+
+  const content = modalContentData[contentKey];
+  modalContent.innerHTML = content;
+};
+
+const openModal = (contentKey, title) => {
+  updateModalContent(contentKey, title);
+  toggleScroll(true);
+  modal.showModal();
+};
+
+const handleOpenModalClick = (event) => {
+  const button = event.target.closest("[data-modal]");
+
+  if (!button) return;
+
+  const contentKey = button.getAttribute("data-modal");
+  const title = button.getAttribute("data-title");
+  openModal(contentKey, title);
+};
+
+document.addEventListener("click", handleOpenModalClick);
+modal.addEventListener("close", () => toggleScroll(false));
