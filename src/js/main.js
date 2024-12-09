7fd8d93a99902124e6a8567afd75cb5375204aeb
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-12-09 03:06:58 +0200

    add scroll-up button

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,29 @@
+import throttle from "lodash.throttle";
+
 import "./header";
 import "./artist";
 import "./feedback";
 import "./modal";
+
+// -----------------ScrollUp Button----------------------
+const scrollUpButton = document.querySelector(".js-scroll-up-btn");
+
+const threshold = 300;
+
+document.addEventListener(
+  "scroll",
+  throttle(() => {
+    if (window.scrollY > threshold) {
+      scrollUpButton.classList.add("show");
+    } else {
+      scrollUpButton.classList.remove("show");
+    }
+  }, 500)
+);
+
+scrollUpButton.addEventListener("click", () => {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+});
